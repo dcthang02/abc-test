31622d73e219a0465168122d4271dab18ab72fba
dcthang02 <bokimtan123@gmail.com>
2024-09-03 11:33:24 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import "module-alias/register";
+import express, { Request, Response } from "express";
+import { TEXT } from "@/constants/value";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(TEXT);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
